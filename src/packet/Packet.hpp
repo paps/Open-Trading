19c05e6b122c0852c3fa9e8bd3ba201d69007242
paps <paps@trollteam.com>
2012-01-08 02:20:28 +0100

    Removed some useless typedefs.

--- src/packet/Packet.hpp
+++ src/packet/Packet.hpp
@@ -34,25 +34,6 @@
 #include <boost/noncopyable.hpp>
 #include <cstdint>
 
-/*
-#ifdef _WIN32
-#include <BaseTsd.h>
-#include <cstddef>
-#pragma warning(disable: 4355)
-#pragma warning(disable: 4244)
-typedef INT8 int8_t;
-typedef UINT8 uint8_t;
-typedef INT16 int16_t;
-typedef UINT16 uint16_t;
-typedef INT32 int32_t;
-typedef UINT32 uint32_t;
-typedef INT64 int64_t;
-typedef UINT64 uint64_t;
-typedef INT32 ssize_t;
-typedef int socklen_t;
-#endif
-*/
-
 namespace Packet
 {
     enum PacketType
